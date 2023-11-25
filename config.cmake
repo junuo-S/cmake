bbcaaca21da31b25dd075fd1843a8671d61a9b73
junuo <3236877923@qq.com>
2023-11-25 19:42:16 +0800

    将QT目录抽离出去，单独作为一个config.cmake文件

---
+++ config.cmake
@@ -0,0 +1,3 @@
+﻿set(Qt5_DIR "C:/ProgramFiles/Qt/5.15.2/msvc2019_64/lib/cmake/Qt5")
+list(APPEND CMAKE_LIBRARY_PATH "C:/ProgramFiles/Qt/5.15.2/msvc2019_64/bin")
+list(APPEND CMAKE_PREFIX_PATH "C:/ProgramFiles/Qt/5.15.2/msvc2019_64/bin")
