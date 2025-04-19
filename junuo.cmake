6741b784474282d5e5be63a7cfcc4b98dac6a9cc
junuo <3236877923@qq.com>
2025-04-19 17:06:36 +0800

    初步完成junuo第2代cmake结构设计

--- junuo.cmake
+++ junuo.cmake
@@ -3,169 +3,128 @@
     source_group("${source_path}" FILES "${source}")
 endfunction(assign_source_group)
 
-function(junuo_add_executable target_name)
-    foreach(source ${ARGN})
-        assign_source_group(${source})
-    endforeach()
-    add_executable(${ARGV})
-endfunction(junuo_add_executable)
-
-function(junuo_add_library target_name)
-    foreach(source ${ARGN})
-        assign_source_group(${source})
-    endforeach()
-    add_library(${ARGV})
-    set_target_properties(${target_name} PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS true)
-endfunction(junuo_add_library)
-
-function(junuo_use_Qt target)
-    if(NOT target)
-        message(FATAL_ERROR "Missing 'TARGET' argument in junuo_need_Qt function.")
+macro(junuo_package package_name package_type)
+    set(junuo_current_package_name ${package_name})
+    if (${package_type} STREQUAL "EXECUTABLE")
+        if (OS_WIN)
+            add_executable(${junuo_current_package_name} WIN32)
+        elseif (OS_DARWIN)
+            add_executable(${junuo_current_package_name} MACOSX_BUNDLE)
+        else()
+            add_executable(${junuo_current_package_name})
+        endif()
+    elseif (${package_type} STREQUAL "CONSOLE")
+        add_executable(${junuo_current_package_name})
+    elseif (${package_type} STREQUAL "STATIC")
+        add_library(${junuo_current_package_name} STATIC)
+    elseif (${package_type} STREQUAL "SHARED")
+        add_library(${junuo_current_package_name} SHARED)
+    else()
+        message(FATAL_ERROR "not support package type: ${package_type}")
     endif()
-    find_package(${Qt_version} COMPONENTS REQUIRED ${ARGN})
+endmacro(junuo_package)
+
+function(junuo_link_packages)
+    target_link_libraries(${junuo_current_package_name} PRIVATE ${ARGN})
+endfunction(junuo_link_packages)
+
+function(junuo_use_packages)
     foreach(arg ${ARGN})
-        target_link_libraries(${target} PRIVATE ${Qt_version}::${arg})
+        if (${arg} MATCHES "Qt")
+            set(regex "^(Qt[0-9]+)([A-Za-z]+)$")
+            string(REGEX MATCH "${regex}" _ "${arg}")
+            set(Qt_part "${CMAKE_MATCH_1}")
+            set(component_part "${CMAKE_MATCH_2}")
+            if (NOT ${Qt_part} STREQUAL "Qt${QT_VERSION_MAJOR}")
+                message(FATAL_ERROR "Qt version is ambiguous")
+            else()
+                junuo_use_Qt(${component_part})
+            endif()
+        else()
+            string(REPLACE "-" "_" func_name "junuo_use_${arg}")
+            if(COMMAND ${func_name})
+                cmake_language(CALL ${func_name})
+            else()
+                message(FATAL_ERROR "Function ${func_name} is not defined.")
+            endif()
+        endif()
+    endforeach()
+endfunction(junuo_use_packages)
+
+function(junuo_use_Qt)
+    find_package("Qt${QT_VERSION_MAJOR}" COMPONENTS REQUIRED ${ARGN})
+    foreach(arg ${ARGN})
+        target_link_libraries(${junuo_current_package_name} PRIVATE Qt${QT_VERSION_MAJOR}::${arg})
     endforeach(arg)
 endfunction(junuo_use_Qt)
 
-function(junuo_use_Python3 target)
-    if(NOT target)
-        message(FATAL_ERROR "Missing 'TARGET' argument in junuo_use_Python3 function.")
-    endif()
+function(junuo_use_python3)
     find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
-    target_include_directories(${target} PRIVATE ${Python3_INCLUDE_DIRS})
-    target_link_libraries(${target} PRIVATE ${Python3_LIBRARY})
-    target_link_directories(${target} PRIVATE ${Python3_LIBRARY_DIRS})
-endfunction(junuo_use_Python3)
+    target_include_directories(${junuo_current_package_name} PRIVATE ${Python3_INCLUDE_DIRS})
+    target_link_libraries(${junuo_current_package_name} PRIVATE ${Python3_LIBRARY})
+    target_link_directories(${junuo_current_package_name} PRIVATE ${Python3_LIBRARY_DIRS})
+endfunction(junuo_use_python3)
 
-function(junuo_use_OpenSSL target)
-    if(NOT target)
-        message(FATAL_ERROR "Missing 'TARGET' argument in junuo_use_OpenSSL function.")
-    endif()
+function(junuo_use_openssl)
     find_package(OpenSSL REQUIRED)
-    target_link_libraries(${target} PRIVATE OpenSSL::SSL OpenSSL::Crypto)
-endfunction(junuo_use_OpenSSL)
+    target_link_libraries(${junuo_current_package_name} PRIVATE OpenSSL::SSL OpenSSL::Crypto)
+endfunction(junuo_use_openssl)
 
-function(junuo_use_lz4 target)
-    if(NOT target)
-        message(FATAL_ERROR "Missing 'TARGET' argument in junuo_use_lz4 function.")
-    endif()
+function(junuo_use_lz4)
     find_package(lz4 CONFIG REQUIRED)
-    target_link_libraries(${target} PRIVATE lz4::lz4)
+    target_link_libraries(${junuo_current_package_name} PRIVATE lz4::lz4)
 endfunction(junuo_use_lz4)
 
-function(junuo_use_protobuf target)
-    if(NOT target)
-        message(FATAL_ERROR "Missing 'TARGET' argument in junuo_use_protobuf function.")
-    endif()
+function(junuo_use_protobuf)
     find_package(Protobuf CONFIG REQUIRED)
-    target_link_libraries(${target} PRIVATE protobuf::libprotoc protobuf::libprotobuf protobuf::libprotobuf-lite)
+    target_link_libraries(${junuo_current_package_name} PRIVATE protobuf::libprotoc protobuf::libprotobuf protobuf::libprotobuf-lite)
 endfunction(junuo_use_protobuf)
 
-function(junuo_add_generate_sources target GenerateFile)
-    target_sources(${target} PRIVATE ${GenerateFile})
-    source_group("Generate Files" FILES ${GenerateFile})
-endfunction(junuo_add_generate_sources)
-
-function(junuo_auto_moc target_name)
-    set(auto_moc_MOC_SOURCES)
-    # 为每个头文件调用 MOC 并生成对应的源文件
-    foreach(header ${ARGN})
-        get_filename_component(file_extension ${header} EXT)
-        if(${file_extension} STREQUAL ".h")
-            get_filename_component(header_name ${header} NAME_WE)
-            set(moc_output "${CMAKE_CURRENT_BINARY_DIR}/moc/moc_${header_name}.cpp")
-            add_custom_command(
-                OUTPUT ${moc_output}
-                COMMAND ${Qt_version}::moc ${CMAKE_CURRENT_SOURCE_DIR}/${header} -o ${moc_output}
-                DEPENDS ${header}
-            )
-            list(APPEND auto_moc_MOC_SOURCES ${moc_output})
+function(junuo_sources)
+    set(source_file_list)
+    set(ui_file_list)
+    set(qrc_file_list)
+    set(proto_file_list)
+    set(ts_file_list)
+    foreach(_file ${ARGN})
+        get_filename_component(file_extension ${_file} EXT)
+        if (NOT file_extension)
+            continue()
         endif()
-    endforeach()
-    # 将所有的MOC文件合并成一个文件
-    set(ret_file ${CMAKE_CURRENT_BINARY_DIR}/moc/automoc2_0_0.cpp)
-    add_custom_command(
-        OUTPUT ${ret_file}
-        COMMAND ${CMAKE_COMMAND} -E cat ${auto_moc_MOC_SOURCES} > ${ret_file}
-        DEPENDS ${auto_moc_MOC_SOURCES}
-    )
-    target_sources(${target_name} PRIVATE ${ret_file})
-    source_group("Generate Files" FILES ${ret_file})
-endfunction(junuo_auto_moc)
-
-function(junuo_auto_uic target_name)
-    foreach(ui_file ${ARGN})
-        get_filename_component(file_extension ${ui_file} EXT)
-        if(${file_extension} STREQUAL ".ui")
-            # 获取.ui文件的文件名（不包含扩展名）
-            get_filename_component(ui_name ${ui_file} NAME_WE)
-            # 设置输出的头文件路径，通常将它们放在一个单独的目录中，如"ui_headers"
-            set(output_header "${CMAKE_CURRENT_BINARY_DIR}/uic/ui_${ui_name}.h")
-            # 将输出头文件添加到UI_HEADERS列表中，用于后续处理
-            list(APPEND ${UIC_FILE} ${output_header})
-            # 执行uic命令来处理.ui文件并生成头文件
-            add_custom_command(
-                OUTPUT ${output_header}
-                COMMAND ${Qt_version}::uic ${CMAKE_CURRENT_SOURCE_DIR}/${ui_file} -o ${output_header}
-                DEPENDS ${ui_file}
-            )
-            target_sources(${target_name} PRIVATE ${output_header})
-            source_group("Generate Files" FILES ${output_header})
+        if (".h;.cpp;.hpp;.cc" MATCHES ${file_extension})
+            list(APPEND source_file_list ${_file})
+        elseif (${file_extension} STREQUAL ".ui")
+            list(APPEND ui_file_list ${_file})
+        elseif (${file_extension} STREQUAL ".qrc")
+            list(APPEND qrc_file_list ${_file})
+        elseif (${file_extension} STREQUAL ".proto")
+            list(APPEND proto_file_list ${_file})
+        elseif (${file_extension} STREQUAL ".ts")
+            list(APPEND ts_file_list ${_file})
         endif()
+        target_sources(${junuo_current_package_name} PRIVATE ${_file})
+        assign_source_group(${_file})
     endforeach()
-endfunction(junuo_auto_uic)
-
-function(junuo_auto_rcc target_name qrcfile)
-    get_filename_component(file_extension ${qrcfile} EXT)
-    if(${file_extension} STREQUAL ".qrc")
-        get_filename_component(header_name ${qrcfile} NAME_WE)
-        set(rcc_output "${CMAKE_CURRENT_BINARY_DIR}/rcc/rcc_${header_name}.cpp")
-        add_custom_command(
-            OUTPUT ${rcc_output}
-            COMMAND ${Qt_version}::rcc ${CMAKE_CURRENT_SOURCE_DIR}/${qrcfile} -o ${rcc_output}
-            DEPENDS ${qrcfile} ${ARGN}
-        )
-        target_sources(${target_name} PRIVATE ${rcc_output})
-        source_group("Generate Files" FILES ${rcc_output})
+    junuo_deal_ui_files(${ui_file_list})
+    if("junuo_moc" IN_LIST ARGN)
+        junuo_deal_moc_files(${source_file_list})
     endif()
-endfunction(junuo_auto_rcc)
+    junuo_deal_qrc_files(${qrc_file_list})
+    junuo_deal_protobuf_files(${proto_file_list})
+    junuo_add_translation(${ts_file_list})
+endfunction(junuo_sources)
 
-function(junuo_include_directories target_name)
-    target_include_directories(${target_name} PRIVATE ${ARGN})
-endfunction(junuo_include_directories)
-
-function(junuo_link_libraries target_name)
-    target_link_libraries(${target_name} PRIVATE ${ARGN})
-endfunction(junuo_link_libraries)
-
-function(junuo_compile_definitions target_name)
-    target_compile_definitions(${target_name} PRIVATE ${ARGN})
-endfunction(junuo_compile_definitions)
-
-function(junuo_add_translation target_name)
-    find_package(${Qt_version} COMPONENTS LinguistTools REQUIRED)
-    foreach(ts_file ${ARGN})
-        get_filename_component(file_extension ${ts_file} EXT)
-        if(${file_extension} STREQUAL ".ts")
-            get_filename_component(header_name ${ts_file} NAME_WE)
-            add_custom_command(
-                TARGET ${target_name} POST_BUILD
-                COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/translation
-                COMMAND ${Qt_version}::lrelease ${CMAKE_CURRENT_SOURCE_DIR}/${ts_file} -qm ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/translation/${header_name}.qm
-                DEPENDS ${ARGN}
-            )
-        endif()
-    endforeach()
-endfunction(junuo_add_translation)
-
-function(junuo_generate_protobuf_files target_name)
+function(junuo_deal_protobuf_files)
+    list(LENGTH ARGN argn_count)
+    if (argn_count LESS 0 OR argn_count EQUAL 0)
+        return()
+    endif()
     set(proto_gen_path ${CMAKE_CURRENT_BINARY_DIR}/gen-cpp)
     file(MAKE_DIRECTORY ${proto_gen_path})
     # 初始化输出列表
     set(proto_headers)
     set(proto_sources)
-
+    find_package(Protobuf CONFIG REQUIRED)
     # 遍历传入的 proto 文件列表
     foreach(proto_file ${ARGN})
         # 获取 proto 文件名（不带扩展名）
@@ -190,7 +149,110 @@ function(junuo_generate_protobuf_files target_name)
         list(APPEND proto_headers ${proto_header})
         list(APPEND proto_sources ${proto_source})
     endforeach()
-    target_sources(${target_name} PRIVATE ${proto_headers} ${proto_sources})
-    source_group("Generate Files" FILES ${proto_headers} ${proto_sources})
-endfunction(junuo_generate_protobuf_files)
+    junuo_add_generate_sources(${proto_headers} ${proto_sources})
+endfunction(junuo_deal_protobuf_files)
 
+function(junuo_deal_ui_files)
+    set(output_dir ${CMAKE_CURRENT_BINARY_DIR}/uic)
+    foreach(ui_file ${ARGN})
+        get_filename_component(file_extension ${ui_file} EXT)
+        if(${file_extension} STREQUAL ".ui")
+            # 获取.ui文件的文件名（不包含扩展名）
+            get_filename_component(ui_name ${ui_file} NAME_WE)
+            set(output_header "${output_dir}/ui_${ui_name}.h")
+            # 执行uic命令来处理.ui文件并生成头文件
+            add_custom_command(
+                OUTPUT ${output_header}
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${output_dir}
+                COMMAND Qt${QT_VERSION_MAJOR}::uic ${CMAKE_CURRENT_SOURCE_DIR}/${ui_file} -o ${output_header}
+                DEPENDS ${ui_file}
+            )
+            junuo_add_generate_sources(${output_header})
+        endif()
+    endforeach()
+endfunction(junuo_deal_ui_files)
+
+function(junuo_deal_moc_files)
+    list(LENGTH ARGN argn_count)
+    if (argn_count LESS 0 OR argn_count EQUAL 0)
+        return()
+    endif()
+    set(moc_file_base_name junuo_moc_01.cpp)
+    set(generate_moc_file ${CMAKE_CURRENT_BINARY_DIR}/moc/${moc_file_base_name})
+    set_source_files_properties(${generate_moc_file} PROPERTIES GENERATED TRUE)
+    junuo_add_generate_sources(${generate_moc_file})
+    find_program(qmake_executable NAMES qmake-qt${QT_VERSION_MAJOR} qmake)
+    if (NOT qmake_executable)
+        return()
+    endif()
+    get_filename_component(Qt_bin_dir ${qmake_executable} DIRECTORY)
+    add_custom_command(
+        OUTPUT ${generate_moc_file}
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/moc
+        COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/cmake/script/junuo_cmake_help.py moc --compile "${ARGN}" --source-dir ${CMAKE_CURRENT_SOURCE_DIR} --output-dir ${CMAKE_CURRENT_BINARY_DIR} --qt-bin-dir ${Qt_bin_dir} --output-file ${moc_file_base_name}
+        COMMENT "parallel moc ing..."
+        DEPENDS ${LINES}
+    )
+endfunction(junuo_deal_moc_files)
+
+function(junuo_deal_qrc_files)
+    list(LENGTH ARGN argn_count)
+    if (argn_count LESS 0 OR argn_count EQUAL 0)
+        return()
+    endif()
+    set(output_dir ${CMAKE_CURRENT_BINARY_DIR}/rcc)
+    foreach(qrc_file ${ARGN})
+        execute_process(
+            COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/cmake/script/junuo_cmake_help.py rcc --check ${qrc_file} --source-dir ${CMAKE_CURRENT_SOURCE_DIR}
+            OUTPUT_VARIABLE check_result
+            RESULT_VARIABLE result_value
+        )
+        if (NOT result_value EQUAL 0)
+            message(FATAL_ERROR "failed to execute python script. return code: ${result_value}")        
+        endif()
+        string(REPLACE "\n" ";" LINES ${check_result})
+        get_filename_component(file_name ${qrc_file} NAME_WE)
+        set(generate_rcc_file ${output_dir}/rcc_${file_name}.cpp)
+        junuo_sources(${LINES})
+        junuo_add_generate_sources(${generate_rcc_file})
+        add_custom_command(
+            OUTPUT ${generate_rcc_file}
+            COMMAND ${CMAKE_COMMAND} -E make_directory ${output_dir}
+            COMMAND Qt${QT_VERSION_MAJOR}::rcc ${CMAKE_CURRENT_SOURCE_DIR}/${qrc_file} -o ${generate_rcc_file}
+            DEPENDS ${qrc_file} ${LINES}
+        )
+    endforeach()
+endfunction(junuo_deal_qrc_files)
+
+function(junuo_add_translation)
+    find_package(Qt${QT_VERSION_MAJOR} COMPONENTS LinguistTools REQUIRED)
+    foreach(ts_file ${ARGN})
+        get_filename_component(file_extension ${ts_file} EXT)
+        if(${file_extension} STREQUAL ".ts")
+            get_filename_component(header_name ${ts_file} NAME_WE)
+            add_custom_command(
+                TARGET ${junuo_current_package_name} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/translation
+                COMMAND Qt${QT_VERSION_MAJOR}::lrelease ${CMAKE_CURRENT_SOURCE_DIR}/${ts_file} -qm ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/translation/${header_name}.qm
+                DEPENDS ${ts_file}
+            )
+        endif()
+    endforeach()
+endfunction(junuo_add_translation)
+
+function(junuo_add_generate_sources)
+    list(LENGTH ARGN argn_count)
+    if (argn_count LESS 0 OR argn_count EQUAL 0)
+        return()
+    endif()
+    target_sources(${junuo_current_package_name} PRIVATE ${ARGN})
+    source_group("Generate Files" FILES ${ARGN})
+endfunction(junuo_add_generate_sources)
+
+function(junuo_include_directories)
+    target_include_directories(${junuo_current_package_name} PRIVATE ${ARGN})
+endfunction(junuo_include_directories)
+
+function(junuo_compile_definitions)
+    target_compile_definitions(${junuo_current_package_name} PRIVATE ${ARGN})
+endfunction(junuo_compile_definitions)
---
+++ script/junuo_cmake_help.py
@@ -0,0 +1,177 @@
+﻿import sys
+import os
+import concurrent.futures
+import subprocess
+import re
+from pathlib import Path
+import json
+import hashlib
+
+g_strClass = 'class'
+g_strMethod = 'method'
+g_strContent = 'content'
+g_strSourceDir = 'source-dir'
+g_strOutputDir = 'output-dir'
+g_strOutputFile = 'output-file'
+g_strQtBinDir = 'qt-bin-dir'
+
+def parseCommandLine(commandLineList: list) -> dict:
+    ret = {}
+    ret[g_strClass] = commandLineList[0]
+    ret[g_strMethod] = commandLineList[1][2:]
+    ret[g_strContent] = commandLineList[2].split(';')
+    if '' in ret[g_strContent]:
+        ret[g_strContent].remove('')
+    index = 3
+    while index < len(commandLineList):
+        currentStr = commandLineList[index]
+        if currentStr.startswith('--'):
+            ret[currentStr[2:]] = commandLineList[index + 1]
+        elif currentStr.startswith('-'):
+            ret[currentStr[1:]] = commandLineList[index + 1]
+        index += 2
+    return ret
+
+def calculateMd5(filePath: str) -> str:
+    with open(filePath, 'rb') as file:
+        md5 = hashlib.md5()
+        while True:
+            data = file.read(4096)
+            if not data:
+                break
+            md5.update(data)
+        return md5.hexdigest()
+
+def toStandardPath(path: str) -> str:
+    return path.replace('\\\\', '/').replace('\\', '/')
+
+class JunuoMoc:
+    def __init__(self, argMap: dict):
+        self.checkMethodName = 'check'
+        self.compiltMethodName = 'compile'
+        self.method = argMap[g_strMethod]
+        self.sourceDir = toStandardPath(argMap[g_strSourceDir])
+        self.content = argMap[g_strContent]
+        for i in range(len(self.content)):
+            self.content[i] = toStandardPath(os.path.join(self.sourceDir.rstrip('/'), self.content[i]))
+        if self.method == self.compiltMethodName:
+            self.qtBinDir = toStandardPath(argMap[g_strQtBinDir]).rstrip('/')
+            self.outputDir = toStandardPath(argMap[g_strOutputDir]).rstrip('/') + '/moc'
+            self.outputFileName = self.outputDir + '/' + toStandardPath(argMap[g_strOutputFile])
+            self.mocCacheFileName = self.outputDir + '/junuo_moc.cache'
+            if os.path.exists(self.mocCacheFileName):
+                with open(self.mocCacheFileName, 'r') as file:
+                    self.mocCache = json.load(file)
+            else:
+                self.mocCache = {}
+            self.strMd5Key = 'md5'
+            self.strMocFileKey = 'moc_file'
+
+    def __del__(self):
+        if self.method == self.compiltMethodName:
+            with open(self.mocCacheFileName, 'w') as file:
+                json.dump(self.mocCache, file, indent=4)
+
+    def doWork(self):
+        match self.method:
+            case self.checkMethodName:
+                self.check()
+            case self.compiltMethodName:
+                self.compile()
+
+    def check(self):
+        for absoluteFileName in self.content:
+            if self.isNeedMoc(absoluteFileName):
+                print(os.path.relpath(absoluteFileName, self.sourceDir))
+
+    def compile(self):
+        isNeedCombine = False
+        maxWorkerCount = os.cpu_count()
+        if maxWorkerCount is None:
+            maxWorkerCount = 1
+        with concurrent.futures.ThreadPoolExecutor(max_workers=maxWorkerCount) as executor:
+            for absoluteFileName in self.content:
+                if not self.isNeedReMoc(absoluteFileName):
+                    continue
+                isNeedCombine = True
+                if absoluteFileName in self.mocCache.keys():
+                    mocFileName = self.mocCache[absoluteFileName][self.strMocFileKey]
+                else:
+                    fileNameWithoutExt, _ = os.path.splitext(os.path.basename(absoluteFileName))
+                    mocFileName = '/'.join([self.outputDir, 'moc_' + fileNameWithoutExt + '.moc'])
+                    index = 1
+                    while os.path.exists(mocFileName):
+                        mocFileName = '/'.join([self.outputDir, 'moc_' + fileNameWithoutExt + '_' + str(index) + '.moc'])
+                        index += 1
+                self.mocCache[absoluteFileName] = {self.strMd5Key : calculateMd5(absoluteFileName), self.strMocFileKey : mocFileName}
+                executor.submit(self.moc, absoluteFileName, mocFileName)
+        if not isNeedCombine:
+            return
+        with open(self.outputFileName, 'w') as file:
+            for mocFileName in list(Path(self.outputDir).rglob('*.moc')):
+                with open(mocFileName, 'r') as mocFile:
+                    file.write(mocFile.read())
+
+    def moc(self, absoluteFileName: str, outputFileName: str):
+        mocExecName = 'moc'
+        if os.name == 'nt':
+            mocExecName = 'moc.exe'
+        absoluteMocExecPath = '/'.join([self.qtBinDir, mocExecName])
+        commandLine = [absoluteMocExecPath, absoluteFileName, '-o', outputFileName]
+        subprocess.run(commandLine)
+
+    def isNeedMoc(self, absoluteFileName: str) -> bool:
+        with open(absoluteFileName, 'r', encoding='utf8') as file:
+            line = file.readline()
+            while line:
+                if 'Q_OBJECT' in line:
+                    return True
+                line = file.readline()
+        return False
+
+    def isNeedReMoc(self, absoluteFileName: str) -> bool:
+        if not self.isNeedMoc(absoluteFileName):
+            return False
+        if absoluteFileName not in self.mocCache.keys():
+            return True
+        return self.mocCache[absoluteFileName][self.strMd5Key] != calculateMd5(absoluteFileName)
+
+class JunuoRcc:
+    def __init__(self, argMap: dict):
+        self.checkMethodName = 'check'
+        self.method = argMap[g_strMethod]
+        self.sourceDir = toStandardPath(argMap[g_strSourceDir])
+        self.content = argMap[g_strContent]
+        for i in range(len(self.content)):
+            self.content[i] = toStandardPath(os.path.join(self.sourceDir.rstrip('/'), self.content[i]))
+
+    def doWork(self):
+        match self.method:
+            case self.checkMethodName:
+                self.check()
+
+    def check(self):
+        pattern = re.compile('<file>(.*)</file>')
+        for absoluteFileName in self.content:
+            path = os.path.dirname(absoluteFileName)
+            relPath = os.path.relpath(path, self.sourceDir)
+            with open(absoluteFileName, 'r', encoding='utf8') as file:
+                line = file.readline()
+                while line:
+                    match = pattern.search(line)
+                    if match:
+                        print('/'.join([relPath, match.group(1)]))
+                    line = file.readline()
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        sys.exit(0)
+    with open('D:/1.txt', 'a') as file:
+        file.write(str(sys.argv))
+        file.write('\n')
+    argMap = parseCommandLine(sys.argv[1:])
+    match argMap[g_strClass]:
+        case 'moc':
+            JunuoMoc(argMap).doWork()
+        case 'rcc':
+            JunuoRcc(argMap).doWork()
